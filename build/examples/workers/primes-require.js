cb68576be0219ca817504f2d37901d932ed18a12
Nicolas Morel <incubatio@gmail.com>
2013-02-28 13:41:30 +0100

    fix optim example

--- build/examples/workers/primes-require.js
+++ build/examples/workers/primes-require.js
@@ -11,34 +11,29 @@
 
   require(['rect'], function(Rect) {
     self.onmessage = function(event) {
-      var foundPrime, i, n, primes, x;
+      var foundPrime, i, n, primes, x, _i, _ref, _results;
       if (event.data.todo === 'nextprimes') {
         foundPrime = false;
         n = event.data.start;
         primes = [];
         x = 10000;
-        return {
-          search: (function() {
-            var _i, _ref, _results;
-            _results = [];
-            while (primes.length < 5) {
-              n += 1;
-              for (i = _i = 2, _ref = Math.sqrt(n); 2 <= _ref ? _i <= _ref : _i >= _ref; i = 2 <= _ref ? ++_i : --_i) {
-                if (n % i === 0) {
-                  continue;
-                }
-              }
-              if (x-- < 0) {
-                primes.push(n);
-                self.postMessage(n);
-                _results.push(x = 10000);
-              } else {
-                _results.push(void 0);
-              }
+        _results = [];
+        while (primes.length < 5) {
+          n += 1;
+          for (i = _i = 2, _ref = Math.sqrt(n); 2 <= _ref ? _i <= _ref : _i >= _ref; i = 2 <= _ref ? ++_i : --_i) {
+            if (n % i === 0) {
+              continue;
             }
-            return _results;
-          })()
-        };
+          }
+          if (x-- < 0) {
+            primes.push(n);
+            self.postMessage(n);
+            _results.push(x = 10000);
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
       }
     };
     return self.postMessage(true);
